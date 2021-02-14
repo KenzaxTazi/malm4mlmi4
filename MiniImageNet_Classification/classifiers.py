172075cdec2a52160af224a158ff10675fb26075
rainavyas <sayvaniar@gmail.com>
2021-02-14 19:28:58 +0000

    define num class adaptive finetunable resnet classifier

--- MiniImageNet_Classification/classifiers.py
+++ MiniImageNet_Classification/classifiers.py
@@ -1,3 +1,31 @@
 '''
 Different classifier architectures for image classification
 '''
+
+import torch
+import torch.nn. as nn
+from torchvision import models
+
+class ResnetClassifier(nn.Module):
+    def __init__(self, num_classes, pretrained=False, trained_model=None):
+        super().__init__()
+
+        if pretrained:
+            self.network=trained_model
+            num_ftrs = self.network.fc.in_features
+            self.network.network.fc = nn.Linear(num_ftrs, num_classes)
+        else:
+            self.network=models.resnet18(pretrained=False)
+            self.network.avgpool = nn.AdaptiveAvgPool2d(1)
+            num_ftrs = self.network.fc.in_features
+            self.network.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, xb):
+        '''
+        xb is [N x C x H x W]
+        N: batch size
+        C = Channels
+        H = Height
+        W = Width
+        '''
+        return self.network(xb)
