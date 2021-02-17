16380159635b3c0038689ba17af177b1d9b6913f
rainavyas <sayvaniar@gmail.com>
2021-02-17 20:05:43 +0000

    outer loop training structure in place

--- MiniImageNet_Classification/maml.py
+++ MiniImageNet_Classification/maml.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn.functional as F
 from classifiers import *
+import torch.nn as nn
 
 class MAML_trainer():
-    def __init__(self, classifier):
+    def __init__(self, classifier, optimizer):
         self.classifier = classifier
+        self.optimizer = optimizer
 
     def _inner_loop_train(self, x_support, y_support, alpha):
         '''
@@ -22,7 +24,7 @@ class MAML_trainer():
         return a model copy with updated parameters
         '''
 
-    def outer_loop_train(self, x_supports, y_supports):
+    def outer_loop_train(self, x_supports, y_supports, x_queries, y_queries, alpha=0.01, beta=0.01):
         '''
         Perform single outer loop forward and backward pass of MAML algorithm
 
@@ -32,7 +34,8 @@ class MAML_trainer():
 
         x_supports = [T x K_{support}*N x C x H x W], for inner loop training
         y_supports = [T x K_{support}*N], for inner loop training
-        x_queries = [T x K_{query}*N x C x H x W], for measuring inner loop trained model performance ->
+        x_queries = [T x K_{query}*N x C x H x W], for outer loop update
+        y_queries = [T x K_{query}*N], for outer loop update
 
         T: Number of tasks -> sometimes called episodes
         K: K-shot learning
@@ -42,10 +45,28 @@ class MAML_trainer():
         W: Image Width
         '''
 
+        task_losses = []
+
         # Perform inner loop training per task using support sets
+        for task in range(x_supports.size(0)):
+            updated_classifer = self._inner_loop_train(x_supports[task], y_supports[task], alpha)
 
-        # Collect logit predictions for query sets, using updated params from inner loop
+            # Collect logit predictions for query sets, using model updated params for specific task
+            logits = updated_classifer(x_queries[task])
 
-        # Calculate an overall loss
+            # Update task losses
+            curr_loss = F.cross_entropy(logits, y_queries[task])
+            task_losses.append(curr_loss)
 
         # Backward pass to update meta-model parameters
+        total_loss = torch.cat(task_losses).sum()
+        self.optimizer.zero_grad()
+        total_loss.backward()
+        for param in self.optimizer.param_groups[0]['params']:
+            # Bit of regularisation innit
+            nn.utils.clip_grad_value_(param, 10)
+        self.optimizer.step()
+
+        # Return training accuracy and loss
+        loss = total_loss.item()
+        # Need to use an AvergeMeter class to collect accuracies as we iterate through tasks
