6626eabfbcbb2be7ebee8d95b89c3aa041c211cb
Vyas Raina <sayvaniar@gmail.com>
2021-02-26 09:50:37 +0000

    redifined to inherit meta model for convolution

--- MiniImageNet_Classification/classifiers.py
+++ MiniImageNet_Classification/classifiers.py
@@ -4,28 +4,13 @@ Different classifier architectures for image classification
 
 import torch
 import torch.nn as nn
-from torchvision import models
+from meta_model_blocks import *
 
-class ResnetClassifier(nn.Module):
-    def __init__(self, num_classes, pretrained=False, trained_model=None):
-        super().__init__()
 
-        if pretrained:
-            self.network=trained_model
-            num_ftrs = self.network.fc.in_features
-            self.network.network.fc = nn.Linear(num_ftrs, num_classes)
-        else:
-            self.network=models.resnet18(pretrained=False)
-            self.network.avgpool = nn.AdaptiveAvgPool2d(1)
-            num_ftrs = self.network.fc.in_features
-            self.network.fc = nn.Linear(num_ftrs, num_classes)
-
-    def forward(self, xb):
-        '''
-        xb is [N x C x H x W]
-        N: batch size
-        C = Channels
-        H = Height
-        W = Width
-        '''
-        return self.network(xb)
+class MetaMiniImageNetClassifier(MetaConvModel):
+    def __init__(self, out_features):
+        self.channels = 3
+        self.hidden_size = 64
+        super(MetaMiniImageNetClassifier, self).__init__(self.channels, out_features,
+                                                            hidden_size=self.hidden_size,
+                                                            feature_size=5*5*self.hidden_size)
