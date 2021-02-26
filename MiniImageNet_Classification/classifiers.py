fb90277a0bc75e731a590df7653830c14c4f69f1
Vyas Raina <sayvaniar@gmail.com>
2021-02-26 13:43:11 +0000

    use a meta model definition

--- MiniImageNet_Classification/classifiers.py
+++ MiniImageNet_Classification/classifiers.py
@@ -8,9 +8,8 @@ from meta_model_blocks import *
 
 
 class MetaMiniImageNetClassifier(MetaConvModel):
-    def __init__(self, out_features):
+    def __init__(self, out_features, hidden_size=64):
         self.channels = 3
-        self.hidden_size = 64
         super(MetaMiniImageNetClassifier, self).__init__(self.channels, out_features,
-                                                            hidden_size=self.hidden_size,
-                                                            feature_size=5*5*self.hidden_size)
+                                                            hidden_size=hidden_size,
+                                                            feature_size=5*5*hidden_size)
