32c695b17471571738931c26e8d42ccdecfac170
Vyas Raina <sayvaniar@gmail.com>
2021-02-26 13:42:18 +0000

    It works

--- MiniImageNet_Classification/test_maml.py
+++ MiniImageNet_Classification/test_maml.py
@@ -6,11 +6,14 @@ import torch
 from maml import MAML_trainer
 from classifiers import MetaMiniImageNetClassifier
 
+seed = 1
+torch.manual_seed(seed)
+
 num_classes = 2
-meta_classifier = MetaMiniImageNetClassifier(out_features=num_classes)
+meta_classifier = MetaMiniImageNetClassifier(out_features=num_classes, hidden_size=64)
 meta_classifier.train()
 
-lr = 10
+lr = 0.1
 optimizer = torch.optim.SGD(meta_classifier.parameters(), lr)
 my_trainer = MAML_trainer(meta_classifier, optimizer)
 
@@ -19,8 +22,8 @@ T = 3
 K = 5
 N = num_classes
 C = 3
-H = 10
-W = 10
+H = 84
+W = 84
 
 x_supports = torch.randn(T, K*N, C, H, W)
 y_supports = (torch.rand(size=(T, K*N)) < 0.5).long()
@@ -29,13 +32,14 @@ x_queries = torch.randn(T, K*N, C, H, W)
 y_queries = (torch.rand(size=(T, K*N)) < 0.5).long()
 
 # perform a single outer loop train
+print("--------------------------------")
+for name, param in meta_classifier.named_parameters():
+    if name == 'classifier.weight':
+        print(param.data)
 
-params = meta_classifier.state_dict()
-final_layer_before = params['classifier.weight']
 loss = my_trainer.outer_loop_train(x_supports, y_supports, x_queries, y_queries)
-params = meta_classifier.state_dict()
-final_layer_after = params['classifier.weight']
 
-print(final_layer_before)
-print("------------------")
-print(final_layer_after)
+print("--------------------------------")
+for name, param in meta_classifier.named_parameters():
+    if name == 'classifier.weight':
+        print(param.data)
