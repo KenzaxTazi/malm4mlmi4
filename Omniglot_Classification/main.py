235243b75b5321911289dcddc0e26a17c9b1b072
Kenza Tazi <kt2015@ic.ac.uk>
2021-03-09 21:44:01 +0000

    order of dataprep outputs, moved batch size arg

--- Omniglot_Classification/main.py
+++ Omniglot_Classification/main.py
@@ -13,14 +13,14 @@ meta_batch_size = 10 #32
 conv = True
 
 # Import training data
-xtrain_support, xtrain_query, ytrain_support, ytrain_query = dataprep(meta_batch_size, K, N)
+xtrain_support, ytrain_support, xtrain_query, ytrain_query = dp.dataprep(meta_batch_size, K, N)
   
 # Create model and meta model instances
 classifier = m.Classifier(conv, K, N)
-meta_model = maml.MetaModel(classifier, meta_batch_size, update_lr, K , N)
+meta_model = maml.MetaModel(classifier, update_lr, K, N)
 
 # Train and test
-meta_model.meta_learn(xtrain, ytrain, xval, yval)
+meta_model.meta_learn(xtrain_support, xtrain_query, ytrain_support, ytrain_query)
 
 
 
