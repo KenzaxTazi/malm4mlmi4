0df402ce97f0a04f1cb4a48789177b27ea2e62a5
Kenza Tazi <kt2015@ic.ac.uk>
2021-03-09 16:55:47 +0000

    meta batching

--- Omniglot_Classification/maml.py
+++ Omniglot_Classification/maml.py
@@ -3,6 +3,7 @@
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
+from collections import OrderedDict
 
 
 class MetaModel():
@@ -25,18 +26,17 @@ class MetaModel():
     """
 
 
-    def __init__(self, classifier, meta_batch_size, update_lr, num_updates, num_classes):
+    def __init__(self, classifier, update_lr, num_updates, num_classes):
         
         self.classifier = classifier
         self.update_lr = update_lr
-        self.meta_batch_size = meta_batch_size
         self.num_updates = num_updates
         self.num_classes = num_classes
 
         self.optimizer = torch.optim.Adam(classifier.parameters(), lr=update_lr)
 
     
-    def inner_loop_train(self, x_support, y_support, alpha):
+    def inner_loop_train(self, x_support, y_support):
         '''
         x_support = [K*N x C x H x W]
         y_support = [K*N]
@@ -53,26 +53,28 @@ class MetaModel():
         '''
 
         # Forward pass using support sets
-        logits = self.classifier(x_support)
-        loss = F.cross_entropy(logits, y_support)
+        with torch.enable_grad():
+            logits = self.classifier(x_support)
+            loss = F.cross_entropy(logits, y_support)
+            self.optimizer.zero_grad()
+            loss.backward(retain_graph=True)
 
-        # Copy classifier to store updated params-> we don't want to update the actual meta-model
-        copy_classifier = copy.deepcopy(self.classifier)
-        copy_classifier_params = copy_classifier.named_parameters()
+            updated_params = self.classifier.state_dict()
 
-        # Manual backward pass
-        for name, param in self.classifier.named_parameters():
-            grad = param.grad.data
-            if grad is None:
-                new_param = param
-            else:
-                new_param = param - alpha * grad # gradient descent
-            copy_classifier_params[name] = new_param
+            # Manual update
+            for  (name, param) in self.classifier.named_parameters():
+                grad = param.grad
+                if grad is None:
+                    new_param = param
+                else:
+                    new_param = param - self.update_lr * grad # gradient descent
+                updated_params[name] = new_param
 
-        return copy_classifier
+        #print(updated_params['classifier.weight'])
+        return updated_params
 
 
-    def meta_learn(self, x_supports, y_supports, x_queries, y_queries, alpha=0.01):
+    def meta_learn(self, x_supports, y_supports, x_queries, y_queries):
         '''
         Perform single outer loop forward and backward pass of MAML algorithm
 
@@ -92,23 +94,25 @@ class MetaModel():
         H: Image Height
         W: Image Width
         '''
-        task_losses = []
+  
+        total_loss = torch.zeros(1)
 
         # Perform inner loop training per task using support sets
-        for task in range(x_supports.size(0)):
-            updated_regressor = self.inner_loop_train(x_supports[task], y_supports[task], alpha)
+        for batch in range(x_supports.size(0)):
+            for task in range(x_supports.size(1)):
+                updated_params = self.inner_loop_train(x_supports[batch, task], y_supports[batch, task])
 
-            # Collect predictions for query sets, using model updated params for specific task
-            predictions = updated_regressor(x_queries[task])
+                # Collect logit predictions for query sets, using updated params for specific task
+                logits = self.classifier(x_queries[batch, task], updated_params)
 
-            # Update task losses
-            curr_loss = F.mse_loss(predictions, y_queries[task])
-            task_losses.append(curr_loss)
+                # Update task losses
+                curr_loss = F.cross_entropy(logits, y_queries[batch, task])
+                total_loss = total_loss + curr_loss
 
-        # Backward pass to update meta-model parameters
-        total_loss = torch.stack(task_losses).sum()
-        self.optimizer.zero_grad()
-        total_loss.backward()
+            # Backward pass to update meta-model parameters
+            self.optimizer.zero_grad()
+            total_loss.backward()
+
         for param in self.optimizer.param_groups[0]['params']:
             # Bit of regularisation innit
             nn.utils.clip_grad_value_(param, 10)
@@ -116,6 +120,7 @@ class MetaModel():
 
         # Return training accuracy and loss
         loss = total_loss.item()
+
         # Need to use an AvergeMeter class to collect accuracies as we iterate through tasks
 
 
