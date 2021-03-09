d98d78791412522ace221432acca557bc020e27b
Kenza Tazi <kt2015@ic.ac.uk>
2021-03-09 16:40:24 +0000

    integrated meta library

--- Omniglot_Classification/model.py
+++ Omniglot_Classification/model.py
@@ -4,8 +4,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+from torchmeta.modules import MetaModule, MetaConv2d, MetaLinear
 
-class Classifier(nn.Module):
+
+class Classifier(MetaModule):
 
     def __init__(self, conv, K, N):
 
@@ -16,18 +18,18 @@ class Classifier(nn.Module):
         self.softmax = nn.Softmax(N)
         
         if self.conv == True:
-            self.conv1 = nn.ConvTranspose2d(1, 64, 3) #3x3 convolutions 64 filters
-            self.conv2 = nn.ConvTranspose2d(64, 64, 3)
-            self.conv3 = nn.ConvTranspose2d(64, 64, 3)
-            self.conv4 = nn.ConvTranspose2d(64, 64, 3) 
-            self.fc1 = nn.Linear(64, 64)
+            self.conv1 = MetaConv2d(1, 1, 3) #3x3 convolutions 64 filters
+            self.conv2 = MetaConv2d(1, 1, 3)
+            self.conv3 = MetaConv2d(1, 1, 3)
+            self.conv4 = MetaConv2d(1, 1, 3) 
+            self.fc1 = MetaLinear(64, 64)
 
         else:
-            self.fc1 = nn.Linear(28*28, 256)
-            self.fc2 = nn.Linear(128, 64)
-            self.fc3 = nn.Linear(64, 64)
-            self.fc4 = nn.Linear(64, 64)
-            self.fc5 = nn.Linear(64, 64)
+            self.fc1 = MetaLinear(28*28, 256)
+            self.fc2 = MetaLinear(128, 64)
+            self.fc3 = MetaLinear(64, 64)
+            self.fc4 = MetaLinear(64, 64)
+            self.fc5 = MetaLinear(64, 64)
 
 
     def forward(self, x):
@@ -40,10 +42,10 @@ class Classifier(nn.Module):
         '''
 
         if self.conv == True:
-            x = F.relu(self.bn(self.conv1(x)))
-            x = F.relu(self.bn(self.conv2(x)))
-            x = F.relu(self.bn(self.conv3(x)))
-            x = F.relu(self.bn(self.conv4(x)))
+            x = F.max_pool2d(F.relu(self.bn(self.conv1(x))), kernerl_size=2)
+            x = F.max_pool2d(F.relu(self.bn(self.conv2(x))), kernerl_size=2)
+            x = F.max_pool2d(F.relu(self.bn(self.conv3(x))), kernerl_size=2)
+            x = F.max_pool2d(F.relu(self.bn(self.conv4(x))), kernerl_size=2)
     
             x = self.softmax(self.fc1(x))
 
