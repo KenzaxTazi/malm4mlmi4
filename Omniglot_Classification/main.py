647fb2b1ef9da9f80e421d5c95f7909c7c8e4310
Kenza Tazi <kt2015@ic.ac.uk>
2021-03-10 13:37:05 +0000

    arg order for meta_learn

--- Omniglot_Classification/main.py
+++ Omniglot_Classification/main.py
@@ -5,22 +5,43 @@ import dataprep as dp
 import torch
 import torchvision
 
+"""
+MAML Omniglot Experiment
 
-K = 1  # shots or number of updates
-N = 5  # number of classes per task
-update_lr = 0.4 
-meta_batch_size = 10 #32
-conv = True
-
-# Import training data
-xtrain_support, ytrain_support, xtrain_query, ytrain_query = dp.dataprep(meta_batch_size, K, N)
+    Convolutional 5-way, 1-shot omniglot:
+        N = 5
+        K = 1
+        lr = 0.4
+        meta_batch_size = 32
+        conv= True
+        meta_training_iterations = 60000
   
-# Create model and meta model instances
-classifier = m.Classifier(conv, K, N)
-meta_model = maml.MetaModel(classifier, update_lr, K, N)
+    Convolutional 20-way, 5-shot omniglot:
+        N = 20 
+        K = 5
+        lr = 0.1
+        meta_batch_size = 16
+        conv= True
+        meta_training_iterations = 60000         
+"""
 
-# Train and test
-meta_model.meta_learn(xtrain_support, xtrain_query, ytrain_support, ytrain_query)
+if __name__ == '__main__':
+    K = 1
+    N = 5
+    lr = 0.4
+    meta_batch_size = 10  # 32
+    conv = True
+    meta_training_iterations = 60
+
+    # Import training data
+    xtrain_support, ytrain_support, xtrain_query, ytrain_query = dp.dataprep(meta_batch_size, K, N)
+    
+    # Create model and meta model instances
+    classifier = m.Classifier(conv, K, N)
+    meta_model = maml.MetaModel(classifier, lr, K, N)
+
+    # Train and test
+    meta_model.meta_learn(xtrain_support, ytrain_support, xtrain_query, ytrain_query, meta_learn_iterations=meta_training_iterations, train=True)
 
 
 
