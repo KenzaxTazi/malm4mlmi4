fixed params and dim for Softmax

Omniglot_Classification/model.py
@@ -14,8 +14,8 @@ class Classifier(MetaModule):
         super().__init__()
 
         self.conv = conv
-        self.bn = nn.BatchNorm2d(64)
-        self.softmax = nn.Softmax(N)
+        self.bn = nn.BatchNorm2d(64, track_running_stats=False)
+        self.softmax = nn.Softmax(dim=0)
         
         if self.conv == True:
             self.conv1 = MetaConv2d(1, 64, 3, padding=1) #3x3 convolutions 64 filters
@@ -44,19 +44,19 @@ class Classifier(MetaModule):
         '''
 
         if self.conv == True:
-            x = F.max_pool2d(F.relu(self.bn(self.conv1(x, params=self.get_subdict(params,'conv1')))), kernel_size=2)
-            x = F.max_pool2d(F.relu(self.bn(self.conv2(x, params=self.get_subdict(params,'conv2')))), kernel_size=2)
-            x = F.max_pool2d(F.relu(self.bn(self.conv3(x, params=self.get_subdict(params,'conv3')))), kernel_size=2)
-            x = F.max_pool2d(F.relu(self.bn(self.conv4(x, params=self.get_subdict(params,'conv4')))), kernel_size=2)
+            x = F.max_pool2d(F.relu(self.bn(self.conv1(x, params=self.get_subdict(params, 'conv1')))), kernel_size=2)
+            x = F.max_pool2d(F.relu(self.bn(self.conv2(x, params=self.get_subdict(params, 'conv2')))), kernel_size=2)
+            x = F.max_pool2d(F.relu(self.bn(self.conv3(x, params=self.get_subdict(params, 'conv3')))), kernel_size=2)
+            x = F.max_pool2d(F.relu(self.bn(self.conv4(x, params=self.get_subdict(params, 'conv4')))), kernel_size=2)
             x = x.view((x.size(0), -1))  # reshape tensor 
-            x = self.fc1(x, params=self.get_subdict(params,'fc1'))
+            x = self.softmax(self.fc1(x, params=self.get_subdict(params, 'fc1')))
 
         else:
-            x = F.relu(self.bn(self.fc1(x, params=self.get_subdict(params,'fc1'))))
-            x = F.relu(self.bn(self.fc2(x, params=self.get_subdict(params,'fc2'))))
-            x = F.relu(self.bn(self.fc3(x, params=self.get_subdict(params,'fc3'))))
-            x = F.relu(self.bn(self.fc4(x, params=self.get_subdict(params,'fc4'))))
+            x = F.relu(self.bn(self.fc1(x, params=self.get_subdict(params, 'fc1'))))
+            x = F.relu(self.bn(self.fc2(x, params=self.get_subdict(params, 'fc2'))))
+            x = F.relu(self.bn(self.fc3(x, params=self.get_subdict(params, 'fc3'))))
+            x = F.relu(self.bn(self.fc4(x, params=self.get_subdict(params, 'fc4'))))
             x = x.view((x.size(0), -1))  # reshape tensor 
-            x = self.fc5(x, params=self.get_subdict(params,'fc5'))
+            x = self.softmax(self.fc5(x, params=self.get_subdict(params, 'fc5')))
             
         return x