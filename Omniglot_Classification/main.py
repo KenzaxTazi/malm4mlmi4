3655f5c1373dfa5b284fefbf2add9d3b307ef031
Kenza Tazi <kt2015@ic.ac.uk>
2021-03-15 15:40:31 +0000

    include evaluation

--- Omniglot_Classification/main.py
+++ Omniglot_Classification/main.py
@@ -29,17 +29,20 @@ if __name__ == '__main__':
     lr = 0.4
     meta_batch_size = 32
     conv = True
-    epochs = 150
+    epochs = 62
 
-    # Import training data
-    xtrain_support, ytrain_support, xtrain_query, ytrain_query = dp.dataprep(meta_batch_size, K, N)
-  
+    # Import data
+    training_set, validation_set = dp.dataprep(meta_batch_size, K, N)
+    xtrain_support, ytrain_support, xtrain_query, ytrain_query = training_set
+    xval_support, yval_support, xval_query, yval_query = validation_set
+
     # Create model and meta model instances
     classifier = m.Classifier(conv, K, N)
     meta_model = maml.MetaModel(classifier, lr, K, N)
 
-    # Train and test
-    meta_model.meta_learn(xtrain_support, ytrain_support, xtrain_query, ytrain_query, epochs=epochs, train=True)
+    # Train and evaluate
+    train_loss, train_acc  = meta_model.train(xtrain_support, ytrain_support, xtrain_query, ytrain_query, epochs=epochs)
+    val_loss, val_acc  = meta_model.evaluate(xval_support, yval_support, xval_query, yval_query)
 
 
 
