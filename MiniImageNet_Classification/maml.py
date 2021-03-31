2c6e590ad35b04c6a66d5772d8fa42577ea459e7
Kenza Tazi <kt2015@ic.ac.uk>
2021-03-31 23:36:44 +0100

    wrong folder

--- MiniImageNet_Classification/maml.py
+++ MiniImageNet_Classification/maml.py
@@ -43,7 +43,6 @@ class MAML_trainer():
                 else:
                     new_param = param - alpha * grad # gradient descent
                 updated_params[name] = new_param
-                updates_params[name].retain_grad()
 
         return updated_params
 
